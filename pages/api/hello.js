111383170a34b131f024d43ef589195b5d5f20c2
supoa <supoa4470@gmail.com>
2023-01-26 09:28:35 +0600

    ldsjf

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -13,101 +13,101 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export default function handler(req, res) {
+const users = [
+  { name: "sohan", email: "sohanur01744@gmail.com" },
+  {
+    name: "sohanur25800",
+    email: "sohanur25800@gmail.com",
+  },
+  {
+    email: "sohanurrahoman02@gmail.com",
+    name: "test user",
+  },
+  {
+    email: "ranodm@gmail.com",
+    name: "random",
+  },
+  { name: "sohan", email: "sohanur01744@gmail.com" },
+  {
+    name: "sohanur25800",
+    email: "sohanur25800@gmail.com",
+  },
+  {
+    email: "sohanurrahoman02@gmail.com",
+    name: "test user",
+  },
+  {
+    email: "ranodm@gmail.com",
+    name: "random",
+  },
+  { name: "sohan", email: "sohanur01744@gmail.com" },
+  {
+    name: "sohanur25800",
+    email: "sohanur25800@gmail.com",
+  },
+  {
+    email: "sohanurrahoman02@gmail.com",
+    name: "test user",
+  },
+  {
+    email: "ranodm@gmail.com",
+    name: "random",
+  },
+  { name: "sohan", email: "sohanur01744@gmail.com" },
+  {
+    name: "sohanur25800",
+    email: "sohanur25800@gmail.com",
+  },
+  {
+    email: "sohanurrahoman02@gmail.com",
+    name: "test user",
+  },
+  {
+    email: "ranodm@gmail.com",
+    name: "random",
+  },
+  { name: "sohan", email: "sohanur01744@gmail.com" },
+  {
+    name: "sohanur25800",
+    email: "sohanur25800@gmail.com",
+  },
+  {
+    email: "sohanurrahoman02@gmail.com",
+    name: "test user",
+  },
+  {
+    email: "ranodm@gmail.com",
+    name: "random",
+  },
+];
+
+const mailOptionForAnnouncement = (user, content) => {
+  console.log(content[0]);
+  return {
+    from: from,
+    to: `${user.email}`,
+    subject: "New Announcement ",
+    text: `hi ${user.name}`,
+  };
+};
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     // Process a POST request
     console.log(req.body);
     const content = req.body.content;
 
-    const users = [
-      { name: "sohan", email: "sohanur01744@gmail.com" },
-      {
-        name: "sohanur25800",
-        email: "sohanur25800@gmail.com",
-      },
-      {
-        email: "sohanurrahoman02@gmail.com",
-        name: "test user",
-      },
-      {
-        email: "ranodm@gmail.com",
-        name: "random",
-      },
-      { name: "sohan", email: "sohanur01744@gmail.com" },
-      {
-        name: "sohanur25800",
-        email: "sohanur25800@gmail.com",
-      },
-      {
-        email: "sohanurrahoman02@gmail.com",
-        name: "test user",
-      },
-      {
-        email: "ranodm@gmail.com",
-        name: "random",
-      },
-      { name: "sohan", email: "sohanur01744@gmail.com" },
-      {
-        name: "sohanur25800",
-        email: "sohanur25800@gmail.com",
-      },
-      {
-        email: "sohanurrahoman02@gmail.com",
-        name: "test user",
-      },
-      {
-        email: "ranodm@gmail.com",
-        name: "random",
-      },
-      { name: "sohan", email: "sohanur01744@gmail.com" },
-      {
-        name: "sohanur25800",
-        email: "sohanur25800@gmail.com",
-      },
-      {
-        email: "sohanurrahoman02@gmail.com",
-        name: "test user",
-      },
-      {
-        email: "ranodm@gmail.com",
-        name: "random",
-      },
-      { name: "sohan", email: "sohanur01744@gmail.com" },
-      {
-        name: "sohanur25800",
-        email: "sohanur25800@gmail.com",
-      },
-      {
-        email: "sohanurrahoman02@gmail.com",
-        name: "test user",
-      },
-      {
-        email: "ranodm@gmail.com",
-        name: "random",
-      },
-    ];
-
-    const mailOptionForAnnouncement = (user, content) => {
-      console.log(content[0]);
-      return {
-        from: from,
-        to: `${user.email}`,
-        subject: "New Announcement ",
-        text: `hi ${user.name}`,
-      };
-    };
-
     let Failed = [];
     let Success = [];
 
-    var sendMail = function (index) {
+    var sendMail = async function (index) {
       if (index >= users.length) {
         return;
       }
 
       var recipient = users[index];
 
-      transporter.sendMail(
+      await transporter.sendMail(
         mailOptionForAnnouncement(recipient, content),
         function (error, info) {
           if (error) {
@@ -134,6 +134,30 @@ export default function handler(req, res) {
 
     res.status(200).json({ name: "John Doe" });
   } else {
-    // Handle any other HTTP method
+    console.log("not the post request");
+    const { content, name, email } = req.body;
+    console.log({ name, email });
+    let Failed = [];
+    let Success = [];
+    const recipient = { name, email };
+    transporter.sendMail(
+      mailOptionForAnnouncement(recipient, content),
+      function (error, info) {
+        if (error) {
+          console.log(error);
+          Failed.push(recipient.email);
+          console.log(Failed);
+        } else {
+          console.log(
+            "Email sent: " + info.response + "to" + " " + recipient.email
+          );
+
+          Success.push(recipient);
+          console.log({ success: Success.length });
+        }
+      }
+    );
+
+    res.status(200).json({ name: "John Doe" });
   }
 }
